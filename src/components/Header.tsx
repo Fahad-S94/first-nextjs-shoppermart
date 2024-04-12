506fc721ea7bc7cda432827d70cd6a543ca3aea3
Fahad Shoaib <f.shoaib1580@gmail.com>
2024-04-12 02:40:35 -0400

    Update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,66 +1,23 @@
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
   return (
-    <header className="border-b p-4">
-      {/* <Link href="/" classNameName="text-red-700">
-        ShopperMart
-      </Link> */}
-      <nav className="flex items-center justify-between flex-wrap p-6">
-        <div className="flex items-center flex-shrink-0 text-red-700 mr-6">
-          {/* <svg
-            className="fill-current h-8 w-8 mr-2"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg> */}
-          <span className="font-semibold text-xl tracking-tight">ShopperMart</span>
-        </div>
-        <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div className="text-sm lg:flex-grow">
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-            >
-              Docs
-            </a>
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-            >
-              Examples
-            </a>
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-            >
-              Blog
-            </a>
-          </div>
-          <div>
-            <a
-              href="#"
-              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-            >
-              Sign Up
-            </a>
-          </div>
-        </div>
+    <header className="border-b p-4 flex items-center justify-between flex-wrap">
+      <Link
+        href="/"
+        className="font-bold text-red-700 text-3xl tracking-tight"
+      >
+        Shopper<span className='text-blue-700'>M</span>art
+      </Link>
+      <nav className="flex gap-4 *:rounded *:px-2 *:py-1 ">
+        <button className="border border-blue-600 text-blue-600 inline-flex gap-1 items-center">
+          <FontAwesomeIcon icon={faPlus} className="h-3" />
+          <span>Post a Ad</span>
+        </button>
+        <button className="border-r border-l text-gray-600">Sign-Up</button>
+        <button className="bg-blue-800 text-white border-0 px-6">Login</button>
       </nav>
     </header>
   );

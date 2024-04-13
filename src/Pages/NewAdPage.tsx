a9ec0b1f25f40e16f447aff45e3a24c23f144d2a
Fahad Shoaib <f.shoaib1580@gmail.com>
2024-04-13 14:54:07 -0400

    Added form page

--- src/Pages/NewAdPage.tsx
+++ src/Pages/NewAdPage.tsx
@@ -1,5 +1,15 @@
-export default function NewAdPage () {
-    return (
-        
-    )
+export default function NewAdPage() {
+  return (
+    <form>
+      <h1>Form</h1>
+      <input type="text" placeholder="Title" />
+      <input type="text" placeholder="Title" />
+      <select name="" id="">
+        <option selected disabled value=''>Select Category</option>
+        <option value=''>Cars</option>
+        <option value=''>Electronic</option>
+        <option value=''>Properties</option>
+      </select>
+    </form>
+  );
 }

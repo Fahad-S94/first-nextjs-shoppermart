7ad86634cc8534c36f667ef4f390ec2648542e9e
Fahad Shoaib <f.shoaib1580@gmail.com>
2024-04-21 01:13:28 -0400

    added formData as object

--- src/app/newAdForm/page.tsx
+++ src/app/newAdForm/page.tsx
@@ -27,45 +27,49 @@ export default function NewAdPage() {
     }, console.error);
   }
 
-  return (
-    <div>
-      <form className="max-w-xl mx-auto flex gap-12">
-        <div className="grow pt-8">
-          <UploadArea files={files} setFiles={setFiles} />
+  function handleSubmit(formData: FormData) {
+    formData.set('location', JSON.toString(location))
+    formData.set('files', JSON.stringify(files))
+    console.log(Object.fromEntries(formData))
+  }
 
-          <div className="mt-5">
-            <div className="flex justify-between items-center mb-1">
-              <label htmlFor="" className="mt-0 mb-0">
-                Location
-              </label>
-              <div>
-                <button
-                  type="button"
-                  onClick={handleFindMyPositionClick}
-                  className="flex items-center p-1 justify-center text-gray-600 rounded"
-                >
-                  <FontAwesomeIcon icon={faLocationCrosshairs} />
-                </button>
-              </div>
-            </div>
-            <div className="bg-gray-100 overflow-hidden min-h-12 rounded text-gray-400 text-center mt-2">
-              {/* <span className="text-xs">{JSON.stringify(location)}</span> */}
-              <LocationPicker
-                defaultLocation={locationDefault}
-                gpsCoords={gpsCoords}
-                onChange={(location) => setLocation(location)}
-              />
+  return (
+    <form action={handleSubmit} className="max-w-xl mx-auto flex gap-12">
+      <div className="grow pt-8">
+        <UploadArea files={files} setFiles={setFiles} />
+
+        <div className="mt-5">
+          <div className="flex justify-between items-center mb-1">
+            <label htmlFor="" className="mt-0 mb-0">
+              Location
+            </label>
+            <div>
+              <button
+                type="button"
+                onClick={handleFindMyPositionClick}
+                className="flex items-center p-1 justify-center text-gray-600 rounded"
+              >
+                <FontAwesomeIcon icon={faLocationCrosshairs} />
+              </button>
             </div>
           </div>
+          <div className="bg-gray-100 overflow-hidden min-h-12 rounded text-gray-400 text-center mt-2">
+            {/* <span className="text-xs">{JSON.stringify(location)}</span> */}
+            <LocationPicker
+              defaultLocation={locationDefault}
+              gpsCoords={gpsCoords}
+              onChange={(location) => setLocation(location)}
+            />
+          </div>
         </div>
-        <div className="grow pt-3">
-          <FormInputs />
-          <button
-            type="submit"
-            className="bg-blue-600 text-white px-6 py-2 rounded mt-1"
-          ></button>
-        </div>
-      </form>
-    </div>
+      </div>
+      <div className="grow pt-3">
+        <FormInputs />
+        <button
+          type="submit"
+          className="bg-blue-600 text-white px-6 py-2 rounded mt-1"
+        ></button>
+      </div>
+    </form>
   );
 }
--- src/components/FormInputs.tsx
+++ src/components/FormInputs.tsx
@@ -2,27 +2,27 @@ export default function FormInputs() {
   return (
     <>
       <label htmlFor="titleIn">Title</label>
-      <input id="titleIn" type="text" placeholder="Title" />
+      <input name="title" id="titleIn" type="text" placeholder="Title" />
 
       <label htmlFor="priceIn">Price</label>
-      <input id="priceIn" type="number" placeholder="Price" />
+      <input name="price" id="priceIn" type="number" placeholder="Price" />
 
       <label htmlFor="categoryIn">Category</label>
-      <select name="" id="categoryIn" defaultValue="0">
+      <select name="category" id="categoryIn" defaultValue="0">
         <option disabled value="0">
           Select Category
         </option>
-        <option value="">Cars</option>
-        <option value="">Electronic</option>
-        <option value="">Properties</option>
+        <option value="cars">Cars</option>
+        <option value="electronics">Electronics</option>
+        <option value="properties">Properties</option>
       </select>
 
       <label htmlFor="descriptionIn">Description</label>
-      <textarea name="" id="descriptionIn" placeholder="description"></textarea>
+      <textarea name="description" id="descriptionIn" placeholder="description"></textarea>
 
       <label htmlFor="contactIn">Contact Info</label>
       <textarea
-        name=""
+        name="contact"
         id="contactIn"
         placeholder="phone #:438 979 9666"
       ></textarea>
